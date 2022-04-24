7e82db7b922e029dac155ca7bd2aadd07097c175
Mykyta Pochukaiev <mpochukaiev@jappware.com>
2022-04-25 00:11:31 +0300

    gateway redirecting

---
+++ gateway/src/main/java/com/jappware/gateway/GatewayApplication.java
@@ -0,0 +1,29 @@
+package com.jappware.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("ms1", p -> p
+//                        .path("/ms1**")
+//                        .filters(f -> f.rewritePath("/ms1/hello"))
+//                        .uri("http://localhost:8081"))
+//                .route("ms2", p -> p
+//                        .path("/ms2")
+//                        .uri("http://localhost:8082/ms2/hello"))
+//                .build();
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
