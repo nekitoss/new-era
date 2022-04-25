59753f8f36a58b9dd9ef561df7d2da45ee5bd93c
Mykyta Pochukaiev <mpochukaiev@jappware.com>
2022-04-25 13:00:29 +0300

    auto adress lookup

--- MS1/src/main/java/com/jappware/ms1/Controller.java
+++ MS1/src/main/java/com/jappware/ms1/Controller.java
@@ -3,10 +3,12 @@ package com.jappware.ms1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class Controller {
     @GetMapping("/ms1/hello")
-    public String hello() {
-        return "Hello from MS1";
+    public String hello(HttpServletRequest request) {
+        return ("Hello from MS1! \n\r" + request.getRequestURL().toString());
     }
 }
--- MS2/src/main/java/com/jappware/ms2/Controller.java
+++ MS2/src/main/java/com/jappware/ms2/Controller.java
@@ -3,10 +3,12 @@ package com.jappware.ms2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class Controller {
     @GetMapping("/ms2/hello")
-    public String hello() {
-        return "Hello from MS2";
+    public String hello(HttpServletRequest request) {
+        return ("Hello from MS2! \n\r" + request.getRequestURL().toString());
     }
 }
